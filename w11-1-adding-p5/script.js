827d0e964a2b9c177b74b18891c09ef678f7848f
moqn <moonjunghyun@gmail.com>
2022-11-25 14:15:35 +0800

    Initial commit

---
+++ w11-1-adding-p5/script.js
@@ -0,0 +1,14 @@
+function setup() {
+  let cnv = createCanvas(400, 400);
+  cnv.parent("canvasContainer");
+  background(255, 0, 0);
+}
+
+function draw() {
+  //
+}
+
+function buttonClicked() {
+  //alert("Clicked!");
+  background(random(255), random(255), random(255));
+}
